a2d0ff9e3052afc4de23621f3e939e4b7e1d1035
Annabel Adaeze <annabeladaeze@gmail.com>
2022-09-14 17:58:42 +0100

    Seperated routes from logic

---
+++ controllers/books.js
@@ -0,0 +1,45 @@
+import { v4 as uuidv4 } from 'uuid';
+
+let books = []
+
+
+export const getBooks = (req, res) => {
+  res.send(books);
+}
+
+export const createBook = (req, res) =>{
+  const book = req.body;
+  
+   books.push({ ...book, id: uuidv4()});
+  
+  res.send(`Book with the name ${book.bookTitle} added to the database!`); }
+
+  export const getBook = (req, res) =>{
+    const { id } = req.params;
+    
+    const foundBook = books.find(book => book.id == id);
+    
+     res.send(foundBook);
+    }
+
+   export const updateBook =  (req, res) =>{
+      const { id } = req.params;
+      const {bookTitle, authorName, numberOfPages, price } = req.body;
+    
+      const book = books.find((book) => book.id == id);
+    if(bookTitle) book.bookTitle = bookTitle;
+    if(authorName) book.authorName = authorName;
+    if(numberOfPages) book.numberOfPages = numberOfPages;
+    if(price) book.price = price;
+    
+    res.send(`Book with the id ${id} has been updated.`);
+    }
+
+  export const deleteBook = (req, res) =>{
+      const { id } = req.params;
+     
+      books = books.filter((book) => book.id !== id);
+     
+      res.send(`Book with the id ${id} deleted from the database.`);
+     }
+    
--- routes/books.js
+++ routes/books.js
@@ -1,79 +1,18 @@
 import express from 'express';
 
-import { v4 as uuidv4 } from 'uuid';
+import { createBook, getBooks, getBook, deleteBook, updateBook } from '../controllers/books.js';
 
 
 const router = express.Router();
 
-let books = [
-  {
-    bookTitle: "JS Fundamentals",
-  authorName: "John Doe",
-  numberOfPages: "300",
-  price: "$20",
-  ratings: "5 star"
-},
-{
-  bookTitle: "Modern JavaScript",
-  authorName: "Jane Doe",
-  numberOfPages: "400",
-  price: "$30",
-  "ratings": "5 star"
-},
-{
-  bookTitle: "Advanced JavaScript",
-  authorName: "Jonny Doe",
-  numberOfPages: "400",
-  price: "$35",
-  ratings: "5 star"
-}
-]
+router.get('/', getBooks);
 
-router.get('/', (req, res) => {
-console.log(books);
+router.post('/', createBook);
 
-res.send(books);
-});
+router.get('/:id', getBook);
 
-router.get('/:id', (req, res) =>{
+router.patch('/:id', updateBook);
 
-const { id } = req.params;
-
-const foundBooks = books.find(book => book.id == id);
-
-
-  res.send(foundBooks);
-
-});
-
-router.post('/', (req, res) =>{
-const book = (req.body);
-
-
-books.push({ ...book, id: uuidv4()});
-
-res.send(`Book with the name ${book.bookTitle} added to the database!`);
-});
-
-router.patch('/:id', (req, res) =>{
-  const { id } = req.params;
-  const {bookTitle, authorName, numberOfPages, price } = req.body;
-
-  const book = books.find((book) => book.id == id);
-if(bookTitle) book.bookTitle = bookTitle;
-if(authorName) book.authorName = authorName;
-if(numberOfPages) book.numberOfPages = numberOfPages;
-if(price) book.price = price;
-
-res.send(`Book with the id ${id} has been updated.`);
-})
-
-router.delete('/:id', (req, res) =>{
- const { id } = req.params;
-
- books = books.filter((book) => book.id !== id);
-
- res.send(`Book with the id ${id} deleted from the database.`);
-});
+router.delete('/:id', deleteBook);
 
 export default router;
