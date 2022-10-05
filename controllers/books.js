901383e00d4159e439ed192df247ab858d4c60c8
Annabel Adaeze <annabeladaeze@gmail.com>
2022-10-05 13:55:05 +0100

    Indentations updated

--- controllers/books.js
+++ controllers/books.js
@@ -1,23 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
 let books = []
 
-export const getBooks = (req, res) =>{
+export const getBooks = (req, res) => {
       res.send(books);
 }
 
-export const createBook = (req, res)=>{
+export const createBook = (req, res) => {
       const book = req.body;
       books.push({ ...book, id: uuidv4()});
       res.send(`Book with the name ${book.bookTitle} added to the database!`);
 }
 
-export const getBook = (req, res) =>{
+export const getBook = (req, res) => {
       const { id } = req.params;
       const foundBook = books.find(book => book.id ==id);
       res.send(foundBook);
 }
 
-export const updateBook =  (req, res)=>{
+export const updateBook =  (req, res) => {
       const { id } = req.params;
       const {bookTitle, authorName, numberOfPages, price } = req.body;
       const book = books.find((book) => book.id == id);
@@ -32,10 +32,11 @@ export const updateBook =  (req, res)=>{
          book.numberOfPages = numberOfPages;
       }
       if(price){
-      } book.price = price;
+         book.price = price;
+      }
       res.send(`Book with the id ${id} has been updated.`);
 }
-export const deleteBook = (req, res)=> {  
+export const deleteBook = (req, res) => {  
       const { id } = req.params;
       books = books.filter((book) => book.id !== id);
       res.send(`Book with the id ${id} deleted from the database.`); 
